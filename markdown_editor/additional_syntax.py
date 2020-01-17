246f27a07b43ed1361eae1592a1d1825d3683f5d
Christopher Mielack <christopher.mielack@me.com>
2020-01-17 15:27:18 +0100

    implement collecting variable definitions

--- markdown_editor/additional_syntax.py
+++ markdown_editor/additional_syntax.py
@@ -1,4 +1,31 @@
 import re
+from typing import List
 
 re_variable_definition = re.compile('\$([\w_-]+)="([^"]+)"')
+re_variable_insertion = re.compile('\$([\w_-]+)')
 
+
+class MultipleVariableDefinitionsError(BaseException):
+    pass
+
+
+class Doc:
+    def __init__(self, filename="", text=""):
+        self.text: str = text
+        self.filename: str = filename
+
+
+class PreProcessor:
+    """Preprocessor class that analyses all markdown documents for variable definitions, tags and their contents,
+    and replacing their values where they are used.
+    """
+
+    def collect_variable_definitions(self, docs: List[Doc]):
+        definitions = dict()
+        for doc in docs:
+            for variable_name, variable_value in re_variable_definition.findall(doc.text):
+                if variable_name in definitions:
+                    raise MultipleVariableDefinitionsError(f"Variable {repr(variable_name)} is defined multiple times!")
+                definitions[variable_name] = variable_value
+        return definitions
+    
--- markdown_editor/test_additional_syntax.py
+++ markdown_editor/test_additional_syntax.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_editor.additional_syntax import re_variable_definition
+from markdown_editor.additional_syntax import re_variable_definition, re_variable_insertion, PreProcessor, Doc
 
 
 class TestAdditionalSyntax(unittest.TestCase):
@@ -19,4 +19,26 @@ class TestAdditionalSyntax(unittest.TestCase):
             results = re_variable_definition.findall(example)
             self.assertListEqual(expected_kv_pairs, results)
 
-        
+    def test_parsing_variable_insertion_re_works(self):
+        examples = [
+            ('we are using a $variable here!', ['variable'])
+        ]
+
+        for example, expected_variable_names in examples:
+            results = re_variable_insertion.findall(example)
+            self.assertListEqual(expected_variable_names, results)
+
+
+class TestPreProcessor(unittest.TestCase):
+    def test_collecting_variable_definitions(self):
+        pp = PreProcessor()
+        my_docs = [
+            Doc("doc a.md", "#This is some syntax\n $myVar=\"cool var\""),
+            Doc("doc b.md", "#This is some syntax\n $myVar2=\"cool var 2\""),
+        ]
+        variable_definitions = pp.collect_variable_definitions(my_docs)
+        self.assertDictEqual({
+            "myVar":"cool var",
+            "myVar2": "cool var 2"
+        }, variable_definitions)
+
